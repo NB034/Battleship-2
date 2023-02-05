182010aa2339ab55ce2d41088092266bcdb6583f
NB <nb9061@yandex.ru>
2023-02-06 01:40:05 +0500

    Add project files.

---
+++ Battleship 2/App.xaml.cs
@@ -0,0 +1,25 @@
+﻿using Battleship_2.ViewModels;
+using Battleship_2.Views;
+using System.Windows;
+using System.Windows.Navigation;
+
+namespace Battleship_2
+{
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            var viewModel = new MainMenuViewModel();
+            var mainMenuPage = new MainMenuPage(viewModel);
+
+            
+
+            //var viewModel = new MainMenuViewModel();
+            //var view = new MainMenuView(viewModel);
+
+            //view.Show();
+        }
+    }
+}
---
+++ Battleship 2/Views/MainMenuView.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+using Battleship_2.ViewModels;
+
+namespace Battleship_2.Views
+{
+    public partial class MainMenuView : Window
+    {
+        public MainMenuView(MainMenuViewModel viewModel)
+        {
+            InitializeComponent();
+
+            viewModel.ExitButtonAction = this.Close;
+            DataContext = viewModel;
+        }
+    }
+}
